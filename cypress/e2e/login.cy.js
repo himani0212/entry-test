07dc1d49063e7873277b7763835637016781383e
himani0212 <himaniparmar1202@gmail.com>
2025-05-19 21:35:25 -0400

    Add login E2E tests and wrap App in BrowserRouter to fix redirect

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,2 +1,28 @@
-describe('Login Component', () => {
-}) 
+describe('Login Page', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('Displays login form elements', () => {
+    cy.get('input[name="name"]').should('exist');
+    cy.get('input[name="password"]').should('exist');
+    cy.get('button[type="submit"]').should('contain', 'Login');
+  });
+
+  it('Logs in successfully and redirects to Welcome page', () => {
+    cy.get('input[name="name"]').type('admin');
+    cy.get('input[name="password"]').type('password');
+    cy.get('button[type="submit"]').click();
+  });
+
+  it('Fails login with incorrect credentials', () => {
+    cy.get('input[name="name"]').type('wrong');
+    cy.get('input[name="password"]').type('wrongpass');
+    cy.get('button[type="submit"]').click();
+
+    cy.wait(200);
+    cy.get('[data-cy="login-error"]')
+    .should('exist')
+    .and('be.visible')
+    .and('contain', 'Invalid credentials');  });
+});
